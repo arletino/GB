b0f84f940ec82e0f8ac0c1f6c99d74a7eb42b772
arletino <titkov@nxt.ru>
2023-09-17 16:44:37 +0300

    switch to home work

--- seminar/test.py
+++ seminar/test.py
@@ -15,4 +15,13 @@
 # chars = ["a","a","b","b","c","c","c"]
 # print(compress(chars))))
 
-import graphics as gr
+n = 100000000000000
+count = 0
+for i in range(1, int(n ** 0.5) + 1):
+    if i ** 3 <= n:  
+        count +=1
+    if i ** 2 <= n:
+        count +=1
+    if i ** 6 <= n:
+        count -=1
+print(count) 
