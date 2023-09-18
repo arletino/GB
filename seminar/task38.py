f343f86d7008f70876ec5f67bda2d868d0f5448a
arletino <titkov@nxt.ru>
2023-09-18 08:07:50 +0300

    task 9 done

--- seminar/task38.py
+++ seminar/task38.py
@@ -84,7 +84,70 @@ def main():
                 record_info()
 
 main()
+def add_person():
+    name_first = input('Введите имя: ')
+    name_last = input('Введите фамилию: ')
+    phone_num = input('Введите телефон: ')
+    with open('phone_book.txt', 'a', encoding='utf-8') as book:
+        book.write(f'{name_first} {name_last} {phone_num}\n')
 
+def create_file():
+    with open('phone_book.txt', 'w', encoding='utf-8') as book:
+        book.write('Имя   Фамилия   Телефон\n')
+
+def search_name():
+    name_search = input('Введите имя для поиска: ')
+    with open('phone_book.txt', 'r', encoding='utf-8') as book:
+        for line in book:
+            if name_search.lower() == line.strip('\n').split()[0].lower():
+                return line
+        return "Запись не найдена"
+    
+def search_surname():
+    surname_search = input('Введите фамилию для поиска: ')
+    with open('phone_book.txt', 'r', encoding='utf-8') as book:
+        for line in book:
+            if surname_search.lower() == line.strip('\n').split()[1].lower():
+                return line
+        return "Запись не найдена"            
+
+def search_phone():
+    phone_search = input('Введите фамилию для поиска: ')
+    with open('phone_book.txt', 'r', encoding='utf-8') as book:
+        for line in book:
+            if phone_search.lower() == line.strip('\n').split()[2].lower():
+                return line
+        return "Запись не найдена"
+
+def main():
+    print('1) Создать файл телефонной книги ',
+          '2) Добавить запись в телефонную книгу',
+          '3) Найти запись по имени',
+          '4) Найти запись по фамилии',
+          '5) Найти запись по телефону',
+           '6) Выход' , sep='\n', end='\n')
+    match input():
+        case '1':
+            create_file()
+        case '2':
+            add_person()
+        case '3':
+            print(search_name())
+        case '4':
+            print(search_surname())
+        case '5':
+            print(search_phone())
+        case '6':
+            print("Good bye")
+
+# create_file()
+# add_person()
+# add_person()
+# add_person()
+# partytable
+
+
+main()
 
 
 
--- seminar/test.py
+++ seminar/test.py
@@ -1,27 +1,52 @@
-# def compress(chars: list[str]) -> int:
-#     s = ''
-#     letter = ''
-#     count = 1
-#     for ch in chars:
-#         if ch in s:
-#             count +=1
-#         elif count !=1:
-#             s += str(count) + ch
-#             count = 1
-#         else:
-#             s += ch
-#     return s if count == 1 else s + str(count)
+import tkinter as tk
+from tkinter import filedialog as fd
 
-# chars = ["a","a","b","b","c","c","c"]
-# print(compress(chars))))
 
-n = 100000000000000
-count = 0
-for i in range(1, int(n ** 0.5) + 1):
-    if i ** 3 <= n:  
-        count +=1
-    if i ** 2 <= n:
-        count +=1
-    if i ** 6 <= n:
-        count -=1
-print(count) 
+def insert_text():
+    file_name = fd.askopenfilename(
+        filetypes=(("TXT files", "*.txt"),
+                   ("CSV files", "*.csv"),
+                   ("All files", "*.*")))
+    f = open(file_name)
+    s = f.read()
+    tk.text.insert(1.0, s)
+    f.close()
+
+def str_to_sort_list(event):
+    s = ent1.get()
+    s = s.split()
+    s.sort()
+    lab['text'] = ' '.join(s)
+
+root =tk.Tk()
+mainmenu = tk.Menu(root)
+root.config(menu=mainmenu)
+filemenu = tk.Menu(mainmenu, tearoff=0)
+filemenu.add_command(label='Новый')
+filemenu.add_command(label='Открыть', command=insert_text)
+filemenu.add_command(label='Сохранить')
+filemenu.add_separator()
+filemenu.add_command(label='Выход')
+mainmenu.add_cascade(label='Файл', menu=filemenu)
+w = root.winfo_screenwidth() // 4 
+h = root.winfo_screenheight() // 2
+root.geometry(f'400x400-{w}+{h}')
+
+text = tk.IntVar()
+ent1 = tk.Entry(root, width=20, textvariable=text)
+some_text = text.get()
+print(text)
+but = tk.Button(root, text='Hello')
+ent2 = tk.Entry(root, width=20)
+ent2.insert(-1, some_text)
+lab = tk.Label(root, width=20, bg='Red', fg='white')
+
+but.bind('<Button-1>', str_to_sort_list)
+
+ent1.pack()
+
+but.pack()
+lab.pack()
+ent2.pack()
+
+root.mainloop()
